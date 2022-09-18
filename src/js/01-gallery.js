3bff62d428ad28efa6d013dfd177c2d1f7fbafd6
Start579 <103378949+Start579@users.noreply.github.com>
2022-09-18 16:39:32 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
+// // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Добавлена галлерея
+const gallery = document.querySelector(".gallery");
+const image = galleryItems.map(({preview, description, original}) => {
+return `<li class="gallery__item">
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`}).join("");
+
+gallery.insertAdjacentHTML("beforeend", image); 
+// Подключен клик и запрещен переход по ссылке
+gallery.addEventListener("click", onGalleryClick, { once: true });
+
+ function onGalleryClick(evt) {
+    evt.preventDefault();       
+    if(evt.target.nodeName !== "IMG") {
+        return;
+    }
+  var lightbox = new SimpleLightbox('.gallery a', {captionType : "alt", captionsData : "alt", captionDelay : "250"});
+}
