7f225163a4f73e8c369a85540aa806e798591193
Start579 <103378949+Start579@users.noreply.github.com>
2022-09-20 16:49:48 +0300

    12

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
+import Player from "@vimeo/player";
+console.log("Player", Player)
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const SAVE_TIME = 'videoplayer-current-time';
+// ****************************************************
+    const onPlay = function(data) {
+        
+           localStorage.setItem(SAVE_TIME, JSON.stringify(data));         
+        
+        // data is an object containing properties specific to that event
+    };
+    
+    player.on('timeupdate', onPlay);
+    
+      // console.log('play', onPlay);
+// Начало времени воспроизведения**********************
+//     player.getCurrentTime(60).then(function(seconds) {
 
+//         // seconds = the current playback position
+//     }).catch(function(error) {
+//         // an error occurred
+//     });
+// // Локальное Хранилище*********************************
+
+
+    // player.setCurrentTime(seconds).then(function(seconds) {
+    //     // seconds = the actual time that the player seeked to
+    // }).catch(function(error) {
+    //     switch (error.name) {
+    //         case 'RangeError':
+    //             // the time was less than 0 or greater than the video’s duration
+    //             break;
+    
+    //         default:
+    //             // some other error occurred
+    //             break;
+    //     }
+    // });
